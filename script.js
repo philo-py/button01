5a558ca1cf7dae15f63748d96a887f18d0156e38
philo-py <okaimono3776@gmail.com>
2025-06-27 13:45:22 +0900

    1.01

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const btn = document.querySelector('changeBtn');
-const msg = document.querySelector('message');
+const btn = document.querySelector('#changeBtn');
+const msg = document.querySelector('#message');
 
 btn.addEventListener('click', () => {
   msg.textContent = 'ボタンがクリックされました！';
