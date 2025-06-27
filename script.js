f0c64555688c71061ab8db762984f99732b370df
philo-py <okaimono3776@gmail.com>
2025-06-27 14:05:38 +0900

    1.2

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ window.addEventListener('DOMContentLoaded', () =>{
   const btn = document.querySelector('#changeBtn');
   const msg = document.querySelector('#message');
 
+  let count =0; //カウント用の変数
+
   btn.addEventListener('click', () => {
-    msg.textContent = 'ボタンがクリックされました！';
+    count++;
+    msg.textContent = 'ボタンをクリックした回数: ${count}回';
   })
 });
